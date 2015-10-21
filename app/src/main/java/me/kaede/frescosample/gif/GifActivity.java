79f1129db57a4e250bf1d8a8cd210c3460890779
Kaede <kidhaibara@gmail.com>
2015-10-21 18:03:41 +0800

    add play gif usage

---
+++ app/src/main/java/me/kaede/frescosample/gif/GifActivity.java
@@ -0,0 +1,27 @@
+package me.kaede.frescosample.gif;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import me.kaede.frescosample.ImageApi;
+import me.kaede.frescosample.R;
+
+public class GifActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gif);
+
+		SimpleDraweeView draweeView = (SimpleDraweeView) this.findViewById(R.id.drawee_gif);
+		DraweeController controller = Fresco.newDraweeControllerBuilder()
+				.setUri(Uri.parse(ImageApi.other.getUrlByName("does-not-have-this-url")))
+				.setTapToRetryEnabled(true)
+				.setAutoPlayAnimations(true)
+				.build();
+		draweeView.setController(controller);
+	}
+}
