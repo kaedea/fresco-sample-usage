22288fc25f5b4863bd0e937458923165cf13eb06
Kaede <kidhaibara@gmail.com>
2015-10-23 11:15:12 +0800

    change project structure

--- app/src/main/java/me/kaede/frescosample/samplelist/SampleListActivity.java
+++ app/src/main/java/me/kaede/frescosample/samplelist/SampleListActivity.java
@@ -1,86 +1,51 @@
 package me.kaede.frescosample.samplelist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import me.kaede.frescosample.R;
-import me.kaede.frescosample.photoview.PhotoViewActivity;
-import me.kaede.frescosample.recyclerview.RecyclerViewActivity;
-import me.kaede.frescosample.basicusage.BasicUsageActivity;
-import me.kaede.frescosample.gif.GifActivity;
-import me.kaede.frescosample.listener.ListenerActivity;
-import me.kaede.frescosample.listview.ListViewActivity;
-import me.kaede.frescosample.lowres.LowResActivity;
-import me.kaede.frescosample.postprocessor.PostprocessorActivity;
-import me.kaede.frescosample.progressivejpg.ProgressiveJPGActivity;
-import me.kaede.frescosample.resize.ResizeActivity;
 
 public class SampleListActivity extends AppCompatActivity {
-    ActivityHolder activityHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_samplelist);
-        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar_samplelist);
-        this.setSupportActionBar(toolbar);
-        RecyclerView recyclerView = (RecyclerView) this.findViewById(R.id.recyclerview_samplelist);
-        activityHolder = new ActivityHolder();
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        MyAdapter adapter = new MyAdapter();
-        recyclerView.setAdapter(adapter);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-        activityHolder.addActivity("Basic Usage", BasicUsageActivity.class);
-        activityHolder.addActivity("Gif/WebP Animation Image", GifActivity.class);
-        activityHolder.addActivity("LowRes Image", LowResActivity.class);
-        activityHolder.addActivity("Controller Listener", ListenerActivity.class);
-        activityHolder.addActivity("Progressive JPG Streaming", ProgressiveJPGActivity.class);
-        activityHolder.addActivity("Resize Image", ResizeActivity.class);
-        activityHolder.addActivity("Postprocessor", PostprocessorActivity.class);
-        activityHolder.addActivity("ListView", ListViewActivity.class);
-        activityHolder.addActivity("RecyclerView", RecyclerViewActivity.class);
-        activityHolder.addActivity("PhotoView", PhotoViewActivity.class);
-        adapter.notifyDataSetChanged();
+        setContentView(R.layout.activity_recyclerview);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) this.findViewById(R.id.tablayout);
+        ViewPager viewPager = (ViewPager) this.findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        viewPager.setOffscreenPageLimit(1);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    public class MyAdapter extends RecyclerView.Adapter<ViewHolder>{
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_samplelist_recycleview, parent, false);
-            ViewHolder vh = new ViewHolder(v);
-            return vh;
+    public class MyAdapter extends FragmentStatePagerAdapter {
+        public String[] pagers = new String[]{"DraweeView","CustomView"};
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, final int position) {
-            holder.textView.setText(activityHolder.getNameList().get(position));
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    activityHolder.startActivity(v.getContext(),position);
-                }
-            });
+        public Fragment getItem(int position) {
+            return SampleListFragment.newInstance(position);
         }
 
         @Override
-        public int getItemCount() {
-            return activityHolder.getNameList().size();
+        public int getCount() {
+            return pagers.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return pagers[position];
         }
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        public TextView textView;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            textView = (TextView) itemView.findViewById(R.id.tv_item_samplelist_recyclerview);
-        }
-    }
 }
---
+++ app/src/main/java/me/kaede/frescosample/samplelist/SampleListFragment.java
@@ -0,0 +1,132 @@
+package me.kaede.frescosample.samplelist;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import me.kaede.frescosample.ImageApi;
+import me.kaede.frescosample.R;
+import me.kaede.frescosample.basicusage.BasicUsageActivity;
+import me.kaede.frescosample.gif.GifActivity;
+import me.kaede.frescosample.listener.ListenerActivity;
+import me.kaede.frescosample.listview.ListViewActivity;
+import me.kaede.frescosample.lowres.LowResActivity;
+import me.kaede.frescosample.photoview.PhotoViewActivity;
+import me.kaede.frescosample.postprocessor.PostprocessorActivity;
+import me.kaede.frescosample.progressivejpg.ProgressiveJPGActivity;
+import me.kaede.frescosample.recyclerview.MyAdapter;
+import me.kaede.frescosample.recyclerview.RecyclerViewActivity;
+import me.kaede.frescosample.resize.ResizeActivity;
+
+import java.util.List;
+
+/**
+ * Created by kaede on 2015/10/23.
+ */
+public class SampleListFragment extends Fragment{
+	private static final String BUNDLE_INDEX = "BUNDLE_INDEX";
+
+	private int index;
+	ActivityHolder activityHolder;
+
+
+	public static SampleListFragment newInstance(int index) {
+		SampleListFragment fragment = new SampleListFragment();
+		Bundle args = new Bundle();
+		args.putInt(BUNDLE_INDEX, index);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (getArguments() != null) {
+			index = getArguments().getInt(BUNDLE_INDEX);
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_recyclerview, container, false);
+
+
+		//find view
+		RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+
+
+		activityHolder = new ActivityHolder();
+		LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+		recyclerView.setLayoutManager(linearLayoutManager);
+		MyAdapter adapter = new MyAdapter();
+		recyclerView.setAdapter(adapter);
+		recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL_LIST));
+
+		switch (index) {
+			case 0:
+			default:
+				activityHolder.addActivity("Basic Usage", BasicUsageActivity.class);
+				activityHolder.addActivity("Gif/WebP Animation Image", GifActivity.class);
+				activityHolder.addActivity("LowRes Image", LowResActivity.class);
+				activityHolder.addActivity("Controller Listener", ListenerActivity.class);
+				activityHolder.addActivity("Progressive JPG Streaming", ProgressiveJPGActivity.class);
+				activityHolder.addActivity("Resize Image", ResizeActivity.class);
+				activityHolder.addActivity("Postprocessor", PostprocessorActivity.class);
+				activityHolder.addActivity("ListView", ListViewActivity.class);
+				activityHolder.addActivity("RecyclerView", RecyclerViewActivity.class);
+
+				break;
+			case 1:
+				activityHolder.addActivity("PhotoView", PhotoViewActivity.class);
+				break;
+			case 2:
+				break;
+		}
+
+		adapter.notifyDataSetChanged();
+
+
+		return view;
+	}
+
+	public class MyAdapter extends RecyclerView.Adapter<ViewHolder>{
+
+		@Override
+		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_samplelist_recycleview, parent, false);
+			ViewHolder vh = new ViewHolder(v);
+			return vh;
+		}
+
+		@Override
+		public void onBindViewHolder(ViewHolder holder, final int position) {
+			holder.textView.setText(activityHolder.getNameList().get(position));
+			holder.itemView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					activityHolder.startActivity(v.getContext(),position);
+				}
+			});
+		}
+
+		@Override
+		public int getItemCount() {
+			return activityHolder.getNameList().size();
+		}
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder{
+		public TextView textView;
+		public ViewHolder(View itemView) {
+			super(itemView);
+			textView = (TextView) itemView.findViewById(R.id.tv_item_samplelist_recyclerview);
+		}
+	}
+}
