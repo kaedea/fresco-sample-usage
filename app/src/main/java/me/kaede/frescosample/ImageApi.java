84596a92de8ca200eaea6957ffb9c3be644345b8
Kaede <kidhaibara@gmail.com>
2015-10-24 21:03:43 +0800

    update ImageApi

--- app/src/main/java/me/kaede/frescosample/ImageApi.java
+++ app/src/main/java/me/kaede/frescosample/ImageApi.java
@@ -4,10 +4,11 @@ import me.kaede.util.ImageHolder;
 
 /**
  * Created by kaede on 2015/10/21.
+ * sample : http://dn-assets-gitcafe-com.qbox.me/Kaedea/Kaede-Assets/raw/gitcafe-pages/image/girly/jk-01.jpg
  */
 public class ImageApi {
-	public static final String DOMAIN = "http://gh.kaedea.com";
-	private static final String BASE_DIR = "fresco-sample-usage/assets/image";
+	public static final String DOMAIN = "http://dn-assets-gitcafe-com.qbox.me";
+	private static final String BASE_DIR = "Kaedea/Kaede-Assets/raw/gitcafe-pages/image";
 	public static ImageHolder girly = new ImageHolder(DOMAIN, BASE_DIR,"girly","jk-",".jpg",17,"00");
 	public static ImageHolder jk = new ImageHolder(DOMAIN, BASE_DIR,"jk","jk-",".jpg",30,"00");
 	public static ImageHolder legs = new ImageHolder(DOMAIN, BASE_DIR,"legs","legs-",".jpg",29,"00");
