9fc479ef3b952a487370f3db73efe16e1549dad5
Kaede <kidhaibara@gmail.com>
2015-10-24 20:08:50 +0800

    update ImageApi

--- app/src/main/java/me/kaede/frescosample/ImageApi.java
+++ app/src/main/java/me/kaede/frescosample/ImageApi.java
@@ -6,8 +6,10 @@ import me.kaede.util.ImageHolder;
  * Created by kaede on 2015/10/21.
  */
 public class ImageApi {
-	public static ImageHolder girly = new ImageHolder("http://gh.kaedea.com","fresco-sample-usage/assets/image","girly","jk-",".jpg",17,"00");
-	public static ImageHolder jk = new ImageHolder("http://gh.kaedea.com","fresco-sample-usage/assets/image","jk","jk-",".jpg",30,"00");
-	public static ImageHolder legs = new ImageHolder("http://gh.kaedea.com","fresco-sample-usage/assets/image","legs","legs-",".jpg",29,"00");
-	public static ImageHolder other = new ImageHolder("http://gh.kaedea.com","fresco-sample-usage/assets/image","other","",".jpg",Integer.MAX_VALUE,"00");
+	public static final String DOMAIN = "http://gh.kaedea.com";
+	private static final String BASE_DIR = "fresco-sample-usage/assets/image";
+	public static ImageHolder girly = new ImageHolder(DOMAIN, BASE_DIR,"girly","jk-",".jpg",17,"00");
+	public static ImageHolder jk = new ImageHolder(DOMAIN, BASE_DIR,"jk","jk-",".jpg",30,"00");
+	public static ImageHolder legs = new ImageHolder(DOMAIN, BASE_DIR,"legs","legs-",".jpg",29,"00");
+	public static ImageHolder other = new ImageHolder(DOMAIN, BASE_DIR,"other","",".jpg",Integer.MAX_VALUE,"00");
 }
